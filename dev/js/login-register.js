58c61f79d9ee1491ef8c1b72f6947f0dc8541dc1
tucno21 <carlitostucno@gmail.com>
2021-02-20 11:07:47 -0500

    agregando modal

---
+++ dev/js/login-register.js
@@ -0,0 +1,56 @@
+const topbar = document.getElementById('topbar')
+
+const loginModal = document.getElementById('login-modal')
+
+const registerModal = document.getElementById('register-modal')
+
+const loginForm = document.getElementById('login-form')
+const registerForm = document.getElementById('register-form')
+
+if(topbar){
+    topbar.addEventListener('click', (e)=>{
+        if(e.target.parentElement.dataset.type != undefined){
+            if(e.target.parentElement.dataset.type=='login'){
+                loginModal.classList.add('lightbox--show')
+            } else if (e.target.parentElement.dataset.type == 'register'){
+                registerModal.classList.add('lightbox--show')
+            }
+        }
+    })
+}
+
+if(loginModal){
+    loginModal.addEventListener('click', (e)=>{
+        if(e.target.classList.contains('lightbox')){
+            loginModal.classList.remove('lightbox--show')
+        }
+    })
+}
+
+if(registerModal){
+    registerModal.addEventListener('click', (e)=>{
+        if(e.target.classList.contains('lightbox')){
+            registerModal.classList.remove('lightbox--show')
+        }
+    })
+}
+
+if(loginForm){
+    loginForm.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        setTimeout(() => {
+            e.target.button.blur()
+        }, 200);
+        console.log('SEND')
+    })
+}
+
+if(registerForm){
+    loginForm.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        setTimeout(() => {
+            e.target.button.blur()
+        }, 200);
+        console.log('SEND')
+    })
+}
